f47b0473c7e7c3de145863192e15f535764bb05e
Abel Rois <daimonos.wake@gmail.com>
2024-01-26 19:12:20 -0600

    desarrollo del frontend, avance en el backend

---
+++ app/functions/bee_custom_functions.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * Regresa el rol del usuario
+ * 
+ * @return mixed
+ */
+
+function get_user_role(){
+  return $rol = get_user('rol');
+}
+
+function get_default_roles() {
+  return ['root', 'admin']; 
+}
+
+function is_root($rol) {
+  return in_array($rol, ['root']); 
+}
+
+function is_profesor($rol) {
+  return in_array($rol, ['profesor', 'admin', 'root']); 
+}
+
+function is_alumno($rol) {
+  return in_array($rol, ['alumno', 'admin', 'root']); 
+}
+
+function is_user($rol, $roles_aceptados) {
+  $default = get_default_roles();
+
+  if (!is_array($roles_aceptados)) {
+    array_push($default, $roles_aceptados);
+    return in_array($rol, $default);
+  }
+
+  return in_array($rol, array_merge($default, $roles_aceptados));
+}
